607b98c7401e1ec68351f2688571bce36b1a7c08
joandiar91 <joandiar91@gmail.com>
2024-03-08 15:59:34 -0500

    Python para backend: web server con fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact')
+def get_list():
+    return { 'name' : 'Platzi'}
+
+@app.get('/pagina', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Página en servidor</h1>
+        <p>Esta es mi primera página HTML con Python en un servidor </p>
+    """
 
 def run():
     store.get_categories()
